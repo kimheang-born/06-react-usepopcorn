0459ed4f931a2cc09f9df5c5862acabeca75c555
Kimheang <kimheang135@gmail.com>
2023-11-12 14:06:35 +0700

    feat: loading movie details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import StarRating from './StarRating';
 
 const tempMovieData = [
   {
@@ -70,12 +71,14 @@ export default function App() {
   }
 
   useEffect(() => {
-    async function fetchMovies() {
+    const fetchMovies = async () => {
       try {
         setIsLoading(true);
         setError('');
 
-        const res = await fetch(`${backendUrl}${apiKey}&s=${query}`);
+        const res = await fetch(
+          `${backendUrl}/?i=tt3896198&apikey=${apiKey}&s=${query}`
+        );
 
         if (!res.ok) {
           throw new Error('Something went wrong with fetching movies');
@@ -94,7 +97,7 @@ export default function App() {
       } finally {
         setIsLoading(false);
       }
-    }
+    };
 
     if (!query.length) {
       setMovies([]);
@@ -233,12 +236,73 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      setIsLoading(true);
+
+      const res = await fetch(
+        `${backendUrl}/?apikey=${apiKey}&i=${selectedId}`
+      );
+
+      const data = await res.json();
+      setMovie(data);
+      setIsLoading(false);
+    };
+    getMovieDetails();
+  }, [selectedId]);
+
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Post of the ${title} movie`} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐</span>
+                {imdbRating} IMDb rating
+              </p>
+            </div>
+          </header>
+
+          <section>
+            <div className="rating">
+              <StarRating maxRating={10} size={24} />
+            </div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
     </div>
   );
 }
