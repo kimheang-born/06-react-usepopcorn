eece79d2728d70937401c11d72cb69538d3b2d6c
Kimheang <kimheang135@gmail.com>
2023-11-12 17:41:07 +0700

    chore: cleaning up (abort) data fetching

--- src/App.js
+++ src/App.js
@@ -32,13 +32,16 @@ export default function App() {
   }
 
   useEffect(() => {
+    const controller = new AbortController();
+
     const fetchMovies = async () => {
       try {
         setIsLoading(true);
         setError('');
 
         const res = await fetch(
-          `${backendUrl}/?i=tt3896198&apikey=${apiKey}&s=${query}`
+          `${backendUrl}/?i=tt3896198&apikey=${apiKey}&s=${query}`,
+          { signal: controller.signal }
         );
 
         if (!res.ok) {
@@ -52,9 +55,13 @@ export default function App() {
 
         setMovies(data.Search);
         setIsLoading(false);
+        setError('');
       } catch (err) {
         console.log(err.message);
-        setError(err.message);
+
+        if (err.name !== 'AbortError') {
+          setError(err.message);
+        }
       } finally {
         setIsLoading(false);
       }
@@ -65,7 +72,10 @@ export default function App() {
       setError('');
       return;
     }
+
     fetchMovies();
+
+    return () => controller.abort();
   }, [query]);
 
   return (
